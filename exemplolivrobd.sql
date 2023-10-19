4cab8c4e4ef73fab3023ee2478de2cfd6b046d20
Júlia Fernanda de Amaral <147757693+juamaraaal@users.noreply.github.com>
2023-10-19 09:20:13 -0300

    Atualizar o exemplolivrobd.sql

--- exemplolivrobd.sql
+++ exemplolivrobd.sql
@@ -3,22 +3,22 @@ use exemplolivro;
 
 create table livro(
 cd_livro int primary key not null auto_increment,
-nome varchar (45),
-lingua varchar (45),
-ano int
+nome varchar (45) not null,
+lingua varchar (45) not null,
+ano int not null
 );
 create table editora(
 cd_editora int primary key not null auto_increment,
-nome varchar(45),
-telefone varchar (16),
-endereco varchar (100)
+nome varchar(45) not null,
+telefone varchar (16) not null,
+endereco varchar (100) not null
 );
 create table autor(
 cd_autor int primary key not null auto_increment,
-nome varchar (45),
-dataNascimento date,
-paisNascimento varchar (45),
-notaBibliografica text
+nome varchar (45) not null,
+dataNascimento date not null,
+paisNascimento varchar (45) not null,
+notaBibliografica text not null
 );
 create table livro_autor(
 cd_livro int not null,
@@ -28,11 +28,11 @@ constraint foreign key (cd_autor) references autor(cd_autor)
 );
 create table edicao(
 cd_edicao int primary key not null auto_increment,
-ISBN varchar(45),
-preco decimal(10,2),
-ano int,
-numPaginas int,
-quantidade int,
+ISBN varchar(45) not null,
+preco decimal(10,2) not null,
+ano int not null,
+numPaginas int not null,
+quantidade int not null,
 cd_livro int null,
 cd_editora int null,
 constraint foreign key(cd_livro) references livro (cd_livro),
